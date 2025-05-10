834b4161b480339e3031fcbe35616a9226299525
Max-Cool-13 <max-cool-2003@mail.ru>
2025-05-10 18:12:48 +0300

    Для ноута

--- app/database.py
+++ app/database.py
@@ -8,7 +8,7 @@ from sqlalchemy.orm import sessionmaker
 
 SQLALCHEMY_DATABASE_URL = (
     f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}"
-    f"@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
+    f"@my_postgres:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
 )
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
